990d1820a9fff9552071303387789223799c12c3
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2024-06-05 15:45:24 -0300

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -15,9 +15,6 @@ export default function Home() {
             <h1 className="text-xl">deployedbylucas</h1>
             <ul className="flex items-center">
                 <li><BsFillMoonStarsFill className="cursor-pointer text-2xl"></BsFillMoonStarsFill></li>
-                <li>
-                  <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8"href="#">Resume</a>
-                </li>
             </ul>
           </nav>
           <div className="text-center p-10">
