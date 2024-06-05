5662d12e35066104345e1cdbaf7174380ebb40e2
LUCAS MARIANO GOMEZ VILLASUSO <lucas.gomez@mi.unc.edu.ar>
2024-06-05 15:44:12 -0300

    .

--- src/app/page.js
+++ src/app/page.js
@@ -25,7 +25,7 @@ export default function Home() {
             <h3 className="text-2xl py-2">Full Stack Developer</h3>
           </div>
           <div className="text-5xl flex justify-center gap-6">
-            <AiFillLinkedin></AiFillLinkedin>
+            <AiFillLinkedin><a href="https://www.linkedin.com/in/lucas-mariano-gomez-villasuso/" target="_blank"></a></AiFillLinkedin>
             <AiFillGithub></AiFillGithub>
           </div>
         </section>
